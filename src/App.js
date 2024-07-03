fe27e84135c4023887977e949813147c8399219a
Héctor Bolívar <hectorbc338@gmail.com>
2024-07-02 22:54:28 -0400

    clase 1, 2 y parte de la clase 3

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter/>
+      <TodoSearch/>
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButtom/>
+
     </div>
+  );
+}
+
+function TodoCounter(){
+  return(
+    <h1>
+      Has completado 3 de 5 TODOS
+    </h1>
+  );
+}
+
+function TodoItem(){
+  return (
+    <li>
+      <span>V</span>
+      <p>LLorar con la LLorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
